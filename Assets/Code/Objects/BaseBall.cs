d44b77f1c2528c47e5dd8e5c46c373517fc9802d
Edmond Szeto <eszeto831@gmail.com>
2021-10-04 23:59:04 -1000

    clean up

--- Assets/Code/Objects/BaseBall.cs
+++ Assets/Code/Objects/BaseBall.cs
@@ -36,28 +36,32 @@ public class BaseBall : MonoBehaviour
         var paddle = other.gameObject.GetComponentInParent<BasePaddle>();
         if (paddle != null)
         {
-            var paddlePosition = paddle.gameObject.transform.localPosition;
-            var paddleWidth = paddle.Sprite.bounds.size.x;
-            var paddleHeight = paddle.Sprite.bounds.size.y;
-            if (gameObject.transform.localPosition.y <= paddlePosition.y - (paddleHeight / 2))
+            BoxCollider2D collider = paddle.HitBox;
+
+            float top = collider.offset.y + (collider.size.y / 2f) + paddle.transform.localPosition.y;
+            float btm = collider.offset.y - (collider.size.y / 2f) + paddle.transform.localPosition.y;
+            float left = collider.offset.x - (collider.size.x / 2f) + paddle.transform.localPosition.x;
+            float right = collider.offset.x + (collider.size.x / 2f) + paddle.transform.localPosition.x;
+            
+            if (gameObject.transform.localPosition.y <= btm)
             {
                 //Hit was from below the paddle
                 var velocity = GetComponent<Rigidbody2D>().velocity;
                 GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y * -1);
             }
-            if (gameObject.transform.localPosition.y >= paddlePosition.y + (paddleHeight / 2))
+            if (gameObject.transform.localPosition.y >= top)
             {
                 //Hit was from above the paddle
                 var velocity = GetComponent<Rigidbody2D>().velocity;
                 GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y * -1);
             }
-            if (gameObject.transform.localPosition.x <= paddlePosition.x + (paddleWidth / 2))
+            if (gameObject.transform.localPosition.x <= left)
             {
                 //Hit was on left
                 var velocity = GetComponent<Rigidbody2D>().velocity;
                 GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * -1, velocity.y);
             }
-            if (gameObject.transform.localPosition.x >= paddlePosition.x - (paddleWidth / 2))
+            if (gameObject.transform.localPosition.x >= right)
             {
                 //Hit was on right
                 var velocity = GetComponent<Rigidbody2D>().velocity;
@@ -124,46 +128,6 @@ public class BaseBall : MonoBehaviour
                 var velocity = GetComponent<Rigidbody2D>().velocity;
                 GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * -1, velocity.y);
             }
-            /*
-            Vector2 size = m_currentTriggeredCollider.HitBox.size;
-            Vector3 centerPoint = new Vector3(m_currentTriggeredCollider.HitBox.offset.x, m_currentTriggeredCollider.HitBox.offset.y, 0f);
-            Vector3 triggeredColliderPosition = transform.TransformPoint(m_currentTriggeredCollider.HitBox.offset);
-            var triggeredColliderWidth = size.x;
-            var triggeredColliderHeight = size.y;
-            */
-
-            /*
-            var triggeredColliderPosition = m_currentTriggeredCollider.gameObject.transform.localPosition;
-            var triggeredColliderWidth = m_currentTriggeredCollider.HitBox.bounds.size.x;
-            var triggeredColliderHeight = m_currentTriggeredCollider.HitBox.bounds.size.y;
-            */
-
-            /*
-            if (gameObject.transform.localPosition.y <= triggeredColliderPosition.y - (triggeredColliderHeight / 2))
-            {
-                //Hit was from below the brick
-                var velocity = GetComponent<Rigidbody2D>().velocity;
-                GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y * -1);
-            }
-            if (gameObject.transform.localPosition.y >= triggeredColliderPosition.y + (triggeredColliderHeight / 2))
-            {
-                //Hit was from above the brick
-                var velocity = GetComponent<Rigidbody2D>().velocity;
-                GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y * -1);
-            }
-            if (gameObject.transform.localPosition.x <= triggeredColliderPosition.x - (triggeredColliderWidth / 2))
-            {
-                //Hit was on left
-                var velocity = GetComponent<Rigidbody2D>().velocity;
-                GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * -1, velocity.y);
-            }
-            if (gameObject.transform.localPosition.x >= triggeredColliderPosition.x + (triggeredColliderWidth / 2))
-            {
-                //Hit was on right
-                var velocity = GetComponent<Rigidbody2D>().velocity;
-                GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * -1, velocity.y);
-            }
-            */
             
             if (brick != null)
             {
